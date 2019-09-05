367caf64685107214c1abb944a6e6fa7cac795e0
Ryan Wiggins 52 <ryanwiggins52@gmail.com>
2019-09-04 20:11:29 -0400

    made it es6 classes

--- app.js
+++ app.js
@@ -1,76 +1,105 @@
-// book constructor
-function Book (title, author, isbn) {
-  this.title = title
-  this.author = author
-  this.isbn = isbn
+class Book {
+  constructor (title, author, isbn) {
+    this.title = title
+    this.author = author
+    this.isbn = isbn
+  }
 }
 
-// ui constructor - set of prototype methods that add the book to the list
-function UI () {}
-UI.prototype.addBookToList = function (book) {
-  const list = document.getElementById('book-list')
-  const row = document.createElement('tr')
-  row.innerHTML =
-   `<td>${book.title}</td>
-  <td>${book.author}</td>
-  <td>${book.isbn}</td>
-  <td><a href='#' class='delete'>X<a></td>`
+class UI {
+  addBookToList (book) {
+    const list = document.getElementById('book-list')
+    // Create tr element
+    const row = document.createElement('tr')
+    // Insert cols
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class="delete">X<a></td>
+    `
 
-  list.appendChild(row)
+    list.appendChild(row)
+  }
+
+  showAlert (message, className) {
+    // Create div
+    const div = document.createElement('div')
+    // Add classes
+    div.className = `alert ${className}`
+    // Add text
+    div.appendChild(document.createTextNode(message))
+    // Get parent
+    const container = document.querySelector('.container')
+    // Get form
+    const form = document.querySelector('#book-form')
+    // Insert alert
+    container.insertBefore(div, form)
+
+    // Timeout after 3 sec
+    setTimeout(function () {
+      document.querySelector('.alert').remove()
+    }, 3000)
+  }
+
+  deleteBook (target) {
+    if (target.className === 'delete') {
+      target.parentElement.parentElement.remove()
+    }
+  }
+
+  clearFields () {
+    document.getElementById('title').value = ''
+    document.getElementById('author').value = ''
+    document.getElementById('isbn').value = ''
+  }
 }
-// show alert
-UI.prototype.showAlert = function (message, className) {
-  // create div
-  const div = document.createElement('div')
 
-  div.className = `alert ${className}`
-
-  div.appendChild(document.createTextNode(message))
-
-  const container = document.querySelector('.container')
-
-  const form = document.querySelector('#book-form')
-
-  container.insertBefore(div, form)
-  setTimeout(function () {
-    document.querySelector('.alert').remove()
-  }, 3000)
-}
-// clear fields
-UI.prototype.clearFields = function () {
-  document.getElementById('title').value = ''
-  document.getElementById('author').value = ''
-  document.getElementById('isbn').value = ''
-}
-// Event Listeners
+// Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function (e) {
   // Get form values
   const title = document.getElementById('title').value
+
   const author = document.getElementById('author').value
+
   const isbn = document.getElementById('isbn').value
-  // instantiate book
+
+  // Instantiate book
   const book = new Book(title, author, isbn)
 
-  // INstantiate UI
+  // Instantiate UI
   const ui = new UI()
 
-  // validate
+  console.log(ui)
+
+  // Validate
   if (title === '' || author === '' || isbn === '') {
-    // error allert
+    // Error alert
     ui.showAlert('Please fill in all fields', 'error')
   } else {
+    // Add book to list
     ui.addBookToList(book)
 
-    // clear fields
+    // Show success
+    ui.showAlert('Book Added!', 'success')
 
+    // Clear fields
     ui.clearFields()
   }
 
-  // Add book to list
-  ui.addBookToList(book)
+  e.preventDefault()
+})
 
-  // clear fields
+// Event Listener for delete
+document.getElementById('book-list').addEventListener('click', function (e) {
+  // Instantiate UI
+  const ui = new UI()
 
-  ui.clearFields()
+  // Delete book
+  ui.deleteBook(e.target)
+
+  // Show message
+  ui.showAlert('Book Removed!', 'success')
+
   e.preventDefault()
 })
